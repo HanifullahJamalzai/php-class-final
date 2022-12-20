6b61c9497e1fb284ac2ceeb2970ca43983322050
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2022-12-20 11:47:53 +0430

    template customization done

---
+++ config/main.php
@@ -0,0 +1,8 @@
+<?php
+
+function base_url()
+{
+    return "http://127.0.0.1/php_class/ProjectFinal";
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,6 @@
+=========== 
+1. Template Customization
+2. Landing Page and Dashboard (OOP ? Procedural)
+
+
+
